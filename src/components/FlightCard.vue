136cfb07f8d490a6912fbcea8998a6058aa3704b
Benjamin. L. Whelan <whelbe@icloud.com>
2021-07-27 23:01:16 +0100

    store setup
    - tomorrow, find way to alter component data in foreach

---
+++ src/components/FlightCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <img src="../assets/paper-plane.png" alt="Avatar" style="width: 100%" />
+    <div class="container">
+      <h4><b>AMS</b></h4>
+      <p>Amsterdam</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "flight-cards",
+};
+</script>
+
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  max-width: 499px;
+  max-height: 498px;
+  margin: 20px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,26 +1,46 @@
 <template>
-    <h1>Booking</h1>
-    <p v-text="flights"></p>
+  <h1>Booking</h1>
+  <p v-text="$store.state.flights"></p>
+  <div class="flight-cards center">
+    <FlightCard
+      v-for="flight in $store.state.flights"
+      :key="flight.id"
+      :flight="flight"
+    />
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import FlightCard from "../components/FlightCard.vue";
 
 export default {
-    name: "booking",
-    data () {
+  name: "booking",
+  components: {
+    FlightCard,
+  },
+  data() {
     return {
-      flights: []
-    }
+      flights: [],
+    };
   },
-    mounted () {
-        axios
-        .get('http://localhost:8080/flight')
-        .then(response => (this.flights = response.data))
+  mounted() {
+    axios
+      .get("http://localhost:8080/flight")
+      .then((response) => (this.$store.state.flights = response.data));
   },
-}
+};
 </script>
 
-<style>
+<style scoped>
+.flight-cards {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+}
 
+.center {
+  display: flex;
+  justify-content: center;
+}
 </style>
