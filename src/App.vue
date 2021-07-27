1c5a036e40fbfa5115935a53d26a2c4629bbd302
Benjamin. L. Whelan <whelbe@icloud.com>
2021-07-27 20:24:07 +0100

    Basic NavBar with CSS

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <Navigation />
   <router-view />
 </template>
 
 <script>
+import Navigation from './components/Navigation.vue';
 
 export default {
   name: 'App',
-  components: {
+  components: { Navigation
   }
 }
 </script>
@@ -15,10 +16,9 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+html, body { margin: 0; padding: 0; } 
 </style>
